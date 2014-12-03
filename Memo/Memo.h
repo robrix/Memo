aa58d50e35c8c70732eb31927c3de48b3a9cce85
Rob Rix <rob.rix@github.com>
2014-12-02 21:37:55 -0500

    Initial Commit

---
+++ Memo/Memo.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Memo.
+extern double MemoVersionNumber;
+
+/// Project version string for Memo.
+extern const unsigned char MemoVersionString[];
+
